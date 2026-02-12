f95fbc42c0c9e12adcd5e17e03796518a0056fc6
csorn2544 <78349946+csorn2544@users.noreply.github.com>
2026-02-13 00:18:38 +0700

    HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import '@fontsource/inter';
 import '@fontsource/inter/800.css';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/my-resume">
+    <HashRouter basename="/my-resume">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
